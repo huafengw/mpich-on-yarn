aaebe4a0aa8bade8df5473d788093d96c49d6be3
huafengw <fvunicorn@gmail.com>
2017-02-22 14:24:29 +0800

    add tests

--- src/main/java/org/apache/hadoop/mpich/util/Utils.java
+++ src/main/java/org/apache/hadoop/mpich/util/Utils.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.hadoop.mpich.util;
 
 import org.apache.hadoop.mpich.appmaster.pmi.ClientToServerCommand;
@@ -18,8 +35,12 @@ public class Utils {
     if (command != null && !command.equals("")) {
       return ClientToServerCommand.valueOf(command.toUpperCase());
     } else {
-      return ClientToServerCommand.UNRECOGNIZED;
+      String mcmd = kvs.get("mcmd");
+      if (mcmd != null && !mcmd.equals("")) {
+        return ClientToServerCommand.valueOf(command.toUpperCase());
+      }
     }
+    return ClientToServerCommand.UNRECOGNIZED;
   }
 
   public static Map<String, String> parseKeyVals(String msg) throws Exception {
